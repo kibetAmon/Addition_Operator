d68408daae11da0cd714c1ddd71756ce4ffcbff9
KibetAmon <kibetngetich805@gmail.com>
2021-10-06 08:53:21 +0300

    updated variable addition to include the last value

--- addition_2.py
+++ addition_2.py
@@ -4,16 +4,13 @@
 def sumnum(num1, num2):
     addition = 0
     for i in range(num1, num2):
-        addition = addition + i
+        addition = addition + (i + 1)
     return addition
 
-
-print(sumnum(50, 150))
 
 def mainsum():
     a = 50
     b = 150
     print("sum of numbers from 50 to 150 is: ", (a, b, sumnum(a, b)))
-    sumnum(a, b)
 
 mainsum()
